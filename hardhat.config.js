1a07f50b6f0fa84cb4aac4fd1842916ffe584a78
Brenda SC <brenda.saavedra@fondeadora.com>
2022-07-07 14:37:30 -0500

    chore: setup network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [
+        PRIVATE_KEY
+      ],
+    }
+  }
 };
